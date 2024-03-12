39c69b6f78c2cdd1ca5aeb6c662b130c3e7f692f
Mardaaa <martin_tp@live.dk>
2024-03-12 11:22:37 +0100

    updated

--- label_plader.py
+++ label_plader.py
@@ -1,10 +1,11 @@
 import cv2
 import glob
+import keyboard
 
 
 def load_image(image_file, image_file_number):
     image = cv2.imread(image_file[image_file_number])
-    cv2.imshow("Image", image)
+    cv2.imshow(f"Image: {image_file_number+1}", image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     return image  
@@ -12,27 +13,33 @@ def load_image(image_file, image_file_number):
 image_files = glob.glob("King Domino dataset/Cropped and perspective corrected boards/*.jpg")
 image_file_number = 0
 
-while True:
-    key = cv2.waitKey(0)
-    if key == 100:
-        image_file_number += 1
-        print("pressed")
-    if key == 27:
+scores = [] # 2D list: Image number and score
+
+while image_file_number < len(image_files):
+    image = load_image(image_files, image_file_number)
+
+
+    if keyboard.is_pressed('esc'):
         break
 
-print(image_file_number)
+    current_state_d = keyboard.is_pressed('d')
+    if current_state_d and image_file_number < len(image_files):
+        score_input = input(f"Type in score for image.{image_file_number+1}.jpg: ")
+        try:
+            score = int(score_input)
+            scores.append([image_file_number+1, score])
+            image_file_number += 1
+        except ValueError:
+            print("Please enter a valid integer score.")
 
+    
+        
 
-# while image_file_number < len(image_files):
-#     image = load_image(image_files, image_file_number)
-#     key = cv2.waitKey(0)
-#     if key == 27: # ESC
-#         break
-#     elif key == 100: # 'd' key
-#         image_file_number +=1
-#         cv2.destroyAllWindows()  # Close previous image window
-#     elif key == 32: # Space key
-#         image_file_number = (image_file_number - 1) % len(image_files)
-#         cv2.destroyAllWindows()  # Close previous image window
+    
+    current_state_a = keyboard.is_pressed('a')
+    if current_state_a and image_file_number > 0:
+        image_file_number -= 1
 
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
+
+print(scores)
