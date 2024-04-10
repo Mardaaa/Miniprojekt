2d0008e91fa58fa02a0b991ab68ec31dea387232
Mardaaa <martin_tp@live.dk>
2024-04-10 17:44:46 +0200

    moved images to new folder

--- template_matching.py
+++ template_matching.py
@@ -4,3 +4,17 @@ import cv2
 image_files = glob.glob("King Domino dataset/Cropped and perspective corrected boards/*.jpg")
 image = cv2.imread(image_files[0])
 
+def TemplateMatching(image, templates):
+    pass
+
+
+
+
+def main():
+    image_files = glob.glob("King Domino dataset/Cropped and perspective corrected boards/*.jpg")
+    
+    image = cv2.imread(image_files[0], cv2.IMREAD_GRAYSCALE)
+
+
+if __name__ == '__main__':
+    main()
