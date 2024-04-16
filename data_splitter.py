ca7968ca4dc3f43e2015f5dd934445e43047f75a
Mardaaa <martin_tp@live.dk>
2024-04-16 10:22:57 +0200

    made data splitter

---
+++ data_splitter.py
@@ -0,0 +1,30 @@
+import glob
+import random
+import os
+import shutil
+from sklearn.model_selection import train_test_split
+
+
+def data_splitter(image_files, test_size=0.2, random_state=42):
+    # Split the data into training and testing sets
+    train_files, test_files = train_test_split(image_files, test_size=test_size, random_state=random_state)
+
+    # Move training data to "training data" folder
+    for file in train_files:
+        filename = os.path.basename(file)
+        dest_path = os.path.join("training data", filename)
+        os.makedirs("training data", exist_ok=True)
+        shutil.copy(file, dest_path)
+    
+    for file in test_files:
+        filename = os.path.basename(file)
+        dest_path = os.path.join("test data", filename)
+        os.makedirs("test data", exist_ok=True)
+        shutil.copy(file, dest_path)
+
+        
+
+image_files = glob.glob("King Domino dataset/Cropped and perspective corrected boards/*.jpg")
+
+if __name__ == '__main__':
+    data_splitter(image_files)
