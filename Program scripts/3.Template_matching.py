25660da154c95271d6ec04d0671cbcbe40236ea9
Mardaaa <martin_tp@live.dk>
2024-04-20 13:44:39 +0200

    added crown images

--- Program scripts/3.Template_matching.py
+++ Program scripts/3.Template_matching.py
@@ -5,7 +5,8 @@ import numpy as np
 
 # image_files_path = '/Users/jens-jakobskotingerslev/Desktop/Miniprojekt semester 2/King Domino dataset/*.jpg'
 image_files_path = 'King Domino dataset/Cropped and perspective corrected boards/*jpg'
-template_files_path = '/Users/jens-jakobskotingerslev/Desktop/Miniprojekt semester 2/Scripts/Crown images/*.jpg'
+# template_files_path = '/Users/jens-jakobskotingerslev/Desktop/Miniprojekt semester 2/Scripts/Crown images/*.jpg'
+
 
 image_files = glob.glob(image_files_path)
 template_files = glob.glob(template_files_path)
